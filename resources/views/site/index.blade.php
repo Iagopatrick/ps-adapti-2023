a547ecd2f0311471b4fba39ac194383a12c2f66f
Iagopatrick <iagogripp@hotmail.com>
2023-11-28 00:40:32 -0300

    esqueleto do index do site

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,1 +1,66 @@
 {{-- Front-end começa aqui! --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{asset('site/css/style.css')}}" type ="text/css">
+  <title>IP</title>
+</head>
+<body>
+  <main class="main">
+    <div id="lateral-esquerda">
+      <p>lateral esquerda</p>
+    </div>
+
+    <div id="container-cards">  
+        <div id="card-principal" class="card">
+            <img src="./camaleao.jpg" alt="Imagem do camaleão">
+            <div class="nome-card">Nome</div>
+            <div>Curso</div>
+            <div>Descrição</div>
+            <button>Contratado</button>
+        </div>
+        <div id="card-principal" class="card">
+          <img src="./camaleao.jpg" alt="Imagem do aluno">
+          <div class="nome-card">Nome</div>
+          <div>Curso</div>
+          <div>Descrição</div>
+          <button>Contratado</button>
+        </div>
+        <div id="card-principal" class="card">
+          <img src="./camaleao.jpg" alt="Imagem do aluno">
+          <div class="nome-card">Nome</div>
+          <div>Curso</div>
+          <div>Descrição</div>
+          <button>Contratado</button>
+        </div>
+        <div id="card-principal" class="card">
+          <img src="./camaleao.jpg" alt="Imagem do aluno">
+          <div class="nome-card">Nome</div>
+          <div>Curso</div>
+          <div>Descrição</div>
+          <button>Contratado</button>
+        </div>
+        <div id="card-principal" class="card">
+          <img src="./camaleao.jpg" alt="Imagem do aluno">
+          <div class="nome-card">Nome</div>
+          <div>Curso</div>
+          <div>Descrição</div>
+          <button>Contratado</button>
+        </div>
+    </div>
+    
+    
+    <div id="perfil">
+      <nav id="perfil-nav">
+        <div>Meu nome</div>
+        <div>Contratados</div>
+        <div>Contatos</div>
+        <div>config</div>
+      </nav>
+    </div>
+  </main>
+</body>
+</html>
